9273c0cddd4113d15e37e12f287e64cfb497bb09
rubensiregar <rubbento88@gmail.com>
2024-10-05 14:36:52 +0700

    materi 4 no 1

---
+++ exercise_01_04/0104_no1.ts
@@ -0,0 +1,30 @@
+// 1 Triangle print (coba recrusive)
+const tinggi: number = 4;
+
+function printTri(t: number): string {
+    let count: number = 0;
+    let tri: string = "";
+    let digit1: number = 0;
+    let digit2: number = 0;
+
+    for (let i: number = 1; i <= t; i++) {
+        count++;
+        console.log(count);
+        tri += "\n";
+
+        for (let j: number = 1; j <= count; j++) {
+            if (digit2 < 9) {
+                digit2++;
+            } else {
+                digit2 = 0;
+                digit1++;
+            }
+            console.log(`${digit1}${digit2}`);
+            tri += `${digit1}${digit2}`;
+        }
+    }
+    return tri;
+}
+
+let tri: string = printTri(tinggi);
+console.log(tri);
