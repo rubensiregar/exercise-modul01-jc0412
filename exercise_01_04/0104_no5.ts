e433e183919e6a505506ad5eb95697a6d5f02b12
rubensiregar <rubbento88@gmail.com>
2024-10-05 23:20:49 +0700

    bug fix no 5

--- exercise_01_04/0104_no5.ts
+++ exercise_01_04/0104_no5.ts
@@ -1,7 +1,31 @@
 // 5. toArray
-let inputText: string = "Hello World What Is Up?";
+let inputText: string = "Hello World Lets Learn Something New";
+let cobaSlice: string = inputText.charAt(6); //slice index dari 1
+console.log(cobaSlice);
 
 function jadiArray(inText: string): string[] {
-    let outWord: string[] = [];
+    let outWord: string[] = [""];
+    let jumKata: number = 1;
+    let panjang: number = inText.length;
+    for (let i: number = 0; i < panjang; i++) {
+        if (inText.charAt(i) === " ") jumKata++;
+    }
+    console.log(jumKata);
+
+    for (let i: number = 0; i < jumKata; i++) {
+        let panjKata: number = 0;
+        outWord[i + 1] = "";
+        for (let j: number = 0; j < panjang; j++) {
+            panjKata++;
+            if (inText.charAt(j) === " ") {
+                break;
+            }
+            outWord[i] += inText.charAt(j);
+        }
+        inText = inText.slice(panjKata);
+    }
     return outWord;
 }
+
+let textSplit: string[] = jadiArray(inputText);
+console.log(textSplit);
