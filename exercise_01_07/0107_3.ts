e759eb91df79c58ac26d8c360325d413f8ec76b0
rubensiregar <rubbento88@gmail.com>
2024-10-10 15:16:17 +0700

    benerin no 3

--- exercise_01_07/0107_3.ts
+++ exercise_01_07/0107_3.ts
@@ -4,9 +4,16 @@ function mergeClean(
 ) {
     let out: any = [...a];
 
-    // ! menggunakan for...in karena akan mengeluarkan indeks. jika for...of akan mengeluarkan elemen
     for (let i in b) {
-        if (out[i].name !== b[i].name) {
+        let sama = false;
+
+        for (let j in out) {
+            if (b[i].name === out[j].name) {
+                sama = true;
+            }
+        }
+
+        if (!sama) {
             out.push(b[i]);
         }
     }
@@ -17,12 +24,15 @@ function mergeClean(
 let students1: { name: any; email: any }[] = [
     { name: "Student 1", email: "student1@mail.com" },
     { name: "Student 2", email: "student2@mail.com" },
+    { name: "Student 4", email: "student456@mail.com" },
 ];
 
 let students2: { name: any; email: any }[] = [
-    { name: "Student 1", email: "student1@mail.com" },
-    { name: "Student 3", email: "student3@mail.com" },
     { name: "Student 4", email: "student444@mail.com" },
+    { name: "Student 3", email: "student3@mail.com" },
+    { name: "Student 1", email: "student1@mail.com" },
 ];
 
 console.log(mergeClean(students1, students2));
+
+//  menggunakan for...in karena akan mengeluarkan indeks. jika for...of akan mengeluarkan elemen
